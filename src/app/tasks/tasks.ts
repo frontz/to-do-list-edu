686f96eaaa6a5a775df18a30ff4596684cd49c96
tomasz.fraczek <tom.frontz@gmail.com>
2025-12-02 16:42:26 +0100

    Data flow with input and output signals

--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -1,14 +1,16 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, input, OnInit} from '@angular/core';
 import { Task } from "../Task";
 import {TasksService} from '../services/tasks-service';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 import {NgClass} from '@angular/common';
+import {Form} from '../form/form';
 
 @Component({
   selector: 'app-tasks',
   imports: [
     FormsModule,
-    NgClass
+    NgClass,
+    Form
   ],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css',
@@ -16,6 +18,8 @@ import {NgClass} from '@angular/common';
 export class Tasks implements OnInit{
 
   constructor(private taskService: TasksService) { }
+
+  title = input<string>('Zadania');
 
   tasks: Task[] = [];
   newTaskName: string = '';
@@ -40,10 +44,16 @@ export class Tasks implements OnInit{
   }
 
 
-  protected addTask() {
-      let task: Task = {id: this.getMaxTaskId(this.tasks) + 1, name: this.newTaskName, done: false};
-      this.tasks.push(task);
-      this.newTaskName = '';
+  // protected addTask() {
+  //     let task: Task = {id: this.getMaxTaskId(this.tasks) + 1, name: this.newTaskName, done: false};
+  //     this.tasks.push(task);
+  //     this.newTaskName = '';
+  // }
+
+  protected getTaskName($event: string) {
+    this.newTaskName = $event;
+    let task: Task = {id: this.getMaxTaskId(this.tasks) + 1, name: this.newTaskName, done: false};
+    this.tasks.push(task);
   }
 }
 
