b15a48ea0cd0332bb5a329c368df0e1666054d6e
tomasz.fraczek <tom.frontz@gmail.com>
2025-12-02 12:23:08 +0100

    Adding task possibility

--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -1,10 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import { Task } from "../Task";
 import {TasksService} from '../services/tasks-service';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-tasks',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    FormsModule
+  ],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css',
 })
@@ -13,6 +17,7 @@ export class Tasks implements OnInit{
   constructor(private taskService: TasksService) { }
 
   tasks: Task[] = [];
+  protected newTaskName: any;
 
   ngOnInit() {
     this.tasks = this.taskService.getTasks();
@@ -26,5 +31,19 @@ export class Tasks implements OnInit{
     task.done = !task.done;
   }
 
+  getMaxTaskId(tasks: Task[]) {
+    if (!tasks || tasks.length === 0) {
+      return 0; // albo 0 – zależnie od logiki
+    }
+    return Math.max(...tasks.map(t => t.id));
+  }
 
+
+  protected addTask() {
+      let task: Task = {id: this.getMaxTaskId(this.tasks) + 1, name: this.newTaskName, done: false};
+      this.tasks.push(task);
+      this.newTaskName = '';
+  }
 }
+
+export default Tasks;
